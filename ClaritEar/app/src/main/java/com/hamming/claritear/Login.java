71a56096075a0c1b3da1cc33df15871f16642dff
panduwicak <pwicaksono291@gmail.com>
2016-03-22 21:40:31 +0700

    Commit pertama
    java home_activity masih kosong

---
+++ ClaritEar/app/src/main/java/com/hamming/claritear/Login.java
@@ -0,0 +1,112 @@
+package com.hamming.claritear;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.kosalgeek.genasync12.AsyncResponse;
+import com.kosalgeek.genasync12.PostResponseAsyncTask;
+
+import java.util.HashMap;
+
+/**
+ * Kelas Login untuk activity Login
+ */
+public class Login extends AppCompatActivity {
+    EditText inputUsername, inputPassword;
+    Button login;
+
+    /**
+     * Method onCreate untuk mendeklarasi button, edittext, dan textview
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        inputUsername = (EditText)findViewById(R.id.et_login_username);
+        inputPassword = (EditText)findViewById(R.id.et_login_password);
+        login = (Button)findViewById(R.id.btn_login_login);
+    }
+
+    /**
+     * Method login untuk login saat tombol login ditekan
+     * @param v
+     */
+    public void login(View v){
+        HashMap postDataLogin = new HashMap();
+        postDataLogin.put("usernamelogin", inputUsername.getText().toString());
+        postDataLogin.put("passlogin", inputPassword.getText().toString());
+
+        if(inputUsername.getText().toString().equals("")||inputPassword.getText().toString().equals("")){
+            Toast.makeText(this, "One or more field(s) are empty", Toast.LENGTH_LONG).show();
+        }
+        else{
+            String urlLogin = "http://teamhamming.esy.es/Login.php";
+            PostResponseAsyncTask loginTask = new PostResponseAsyncTask(this, postDataLogin, new AsyncResponse() {
+                @Override
+                public void processFinish(String result2) {
+                    switch(result2){
+                        case "true":
+                            //Menyimpan info login berupa username dengan SharedPreferance
+                            /*SharedPreferences sharedPrefLogin = getSharedPreferences("loginInfo", Context
+                                    .MODE_PRIVATE);
+
+                            SharedPreferences.Editor editor = sharedPrefLogin.edit();
+                            editor.putString("email", inputUsername.getText().toString());
+                            editor.apply();*/
+                            Intent login = new Intent(Login.this,HomeActivity.class);
+                            startActivity(login);
+                            finish();
+                            Toast.makeText(Login.this,"You are logged in",Toast.LENGTH_LONG)
+                                    .show();
+                            break;
+
+                        case "false":
+                            Toast.makeText(Login.this, "Invalid password or username", Toast.LENGTH_LONG)
+                                    .show();
+                            break;
+
+                        default:
+                            Toast.makeText(Login.this,"Connection error. Please try again", Toast.LENGTH_LONG)
+                                    .show();
+                            break;
+                    }
+
+                }
+            });
+            loginTask.execute(urlLogin);
+        }
+    }
+
+    /**
+     * Method toRegister untuk pindah ke activity register saat menekan text "Make new account"
+     * @param v
+     */
+    public void toRegister(View v){
+        Intent toRegister = new Intent(Login.this,Registration.class);
+        startActivity(toRegister);
+        finish();
+    }
+
+    /**
+     * Method onBackPressed untuk keluar dari program saat tombol back ditekan
+     */
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
+        Intent exit = new Intent(Intent.ACTION_MAIN);
+        exit.addCategory(Intent.CATEGORY_HOME);
+        exit.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(exit);
+    }
+}
---
+++ ClaritEar/app/src/main/java/com/hamming/claritear/Registration.java
@@ -0,0 +1,149 @@
+package com.hamming.claritear;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.kosalgeek.genasync12.AsyncResponse;
+import com.kosalgeek.genasync12.PostResponseAsyncTask;
+
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Registration class for registration activity
+ */
+public class Registration extends AppCompatActivity {
+    EditText username, email, password, rptpassword;
+    Button register;
+
+    /**
+     * Method onCreate untuk mendeklarasi fungsi button dan editText di activity_registration.xml
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        username = (EditText)findViewById(R.id.et_regist_username);
+        email = (EditText)findViewById(R.id.et_regist_email);
+        password = (EditText)findViewById(R.id.et_regist_password);
+        rptpassword = (EditText)findViewById(R.id.et_regist_repeatPassword);
+        register = (Button)findViewById(R.id.btn_regist_register);
+    }
+
+    /**
+     * Method register untuk menentukan apa yang akan dilakukan bila button register ditekan
+     */
+    public void register(View v){
+        HashMap PostData = new HashMap();
+        PostData.put("username",username.getText().toString());
+        PostData.put("email",email.getText().toString());
+        PostData.put("password",password.getText().toString());
+
+        if(username.getText().toString().equals("") || email.getText().toString().equals("") || password.getText().toString().equals("") || rptpassword.getText().toString().equals("")){
+            Toast.makeText(this,"One ore more field(s) are empty",Toast.LENGTH_LONG).show();
+        }
+        else if(!validateUser(username.getText().toString())){
+            username.setError("Username must contain at least 4 characters");
+            username.requestFocus();
+        }
+        else if(!validateEmail(email.getText().toString())){
+            email.setError("Invalid Email Address");
+            email.requestFocus();
+        }
+        else if(!validatePassword(password.getText().toString())){
+            password.setError("Password must contain at least 6 characters");
+            password.requestFocus();
+        }
+        else if(!password.getText().toString().equals(rptpassword.getText().toString())) {
+            rptpassword.setError("Passwords don't match");
+            rptpassword.requestFocus();
+        }
+        else{
+            String urlRegister = "http://teamhamming.esy.es/Register.php";
+            PostResponseAsyncTask registerTask = new PostResponseAsyncTask(this, PostData, new AsyncResponse() {
+                @Override
+                public void processFinish(String result) {
+                    Toast.makeText(Registration.this,result,Toast.LENGTH_LONG).show();
+                    if(result.equals("You are now registered")) {
+                        Intent i = new Intent(Registration.this, Login.class);
+                        startActivity(i);
+                        finish();
+                    }
+                    else {
+                        Toast.makeText(Registration.this,"Connection error. Please try again!",Toast.LENGTH_LONG)
+                                .show();
+                    }
+                }
+            });
+            registerTask.execute(urlRegister);
+        }
+    }
+
+    /**
+     * Method toLogin untuk pindah ke activity Login saat EditText "Back to login" ditekan
+     * @param v
+     */
+    public void toLogin(View v){
+        Intent toLogin = new Intent(Registration.this,Login.class);
+        startActivity(toLogin);
+        finish();
+    }
+
+    /**
+     * Method validateUser untuk memvalidasi input username
+     * @param stringusr
+     * @return
+     */
+    private boolean validateUser(String stringusr) {
+        //username harus lebih dari 4 karakter
+        if(stringusr.length()<4){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    /**
+     * Method validatePassword untuk memvalidasi input input password
+     * @param stringpass
+     * @return
+     */
+    private boolean validatePassword(String stringpass) {
+        //password harus lebih dari 6 karakter
+        if(stringpass.length()<6){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    /**
+     * Method validateEmail untuk memvalidasi input email
+     * @param stringemail
+     * @return
+     */
+    private boolean validateEmail(String stringemail) {
+        //Menggunakan Regular Expression
+        String emailPattern = "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+";
+        Pattern pattern = Pattern.compile(emailPattern);
+        Matcher matcher = pattern.matcher(stringemail);
+
+        return matcher.matches();
+    }
+}
